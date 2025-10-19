8e81b14b0dfb3118ad12a1cbbea3e2b59ed02526
VectorSpeed <121528699+VectorSpeed@users.noreply.github.com>
2025-10-19 15:23:55 +0800

    Update

--- Init.py
+++ Init.py
@@ -19,34 +19,32 @@ import os,sys
 hshu = os.getenv("CR")
 
 if hshu:
-  try:
-	hsh = json.loads(hshu)
-  except:
-	print("Bad Access ETP")
-	sys.exit()
-  rl={"Authorization": "token {}".format(hsh["hbtk"])}
-  def elm(fp,etl):
-	r=requests.get(fp,headers=rl)
-	print(fp)
-	if r.status_code != 200:
-	  print("Error retreving code {}".format(etl))
-	  sys.exit()
 	try:
-	  exec(base64.b64decode(r.json()['content']).decode('utf-8'),globals())
+		hsh = json.loads(hshu)
 	except:
-	  print("Bad performance {}".format(etl))
-	  sys.exit()
-	return
-  
-  cues=hsh["ufh"].split(",")
-  cpe=hsh["pasc"].split(",")
-  for a,t in enumerate(cues):
-    elm(hsh["hkj"]+t,str(a)+"A")
-  for a,t in enumerate(cpe):
-    elm(hsh["hkj"]+t,str(a)+"B")
-
-  param["chat_id"]=hsh["rtlA"]
-  param["message_thread_id"]=hsh["rtlU"]
-  requests.get("{}/{}/sendMessage".format(hsh["plecd"],hsh["udh"]),param)
+		print("Bad Access ETP")
+		sys.exit()
+	rl={"Authorization": "token {}".format(hsh["hbtk"])}
+	def elm(fp,etl):
+		r=requests.get(fp,headers=rl)
+		print(fp)
+		if r.status_code != 200:
+			print("Error retreving code {}".format(etl))
+		sys.exit()
+		try:
+			exec(base64.b64decode(r.json()['content']).decode('utf-8'),globals())
+		except:
+			print("Bad performance {}".format(etl))
+			sys.exit()
+		return
+	cues=hsh["ufh"].split(",")
+	cpe=hsh["pasc"].split(",")
+	for a,t in enumerate(cues):
+		elm(hsh["hkj"]+t,str(a)+"A")
+	for a,t in enumerate(cpe):
+		elm(hsh["hkj"]+t,str(a)+"B")
+	param["chat_id"]=hsh["rtlA"]
+	param["message_thread_id"]=hsh["rtlU"]
+	requests.get("{}/{}/sendMessage".format(hsh["plecd"],hsh["udh"]),param)
 else:
-  print("Missing hsh")
+	print("Missing hsh")
